b1dd039e4cfbcbd14faa72195e24070a75d52ab0
seomi <seomi.lee@gmail.com>
2024-07-08 23:36:10 +0900

    :hammer: document page에 내용 추가

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,38 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumnetsPage = () => {
-  return <div>This tis a protected DocumentsPage</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/image/empty.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/image/empty.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Notion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumnetsPage;
