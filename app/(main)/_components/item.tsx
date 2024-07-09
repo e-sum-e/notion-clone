bf7047e45e9380a578741c228e1747a0b5926e08
seomi <seomi.lee@gmail.com>
2024-07-10 01:20:11 +0900

    :hammer: document list 만들기

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -10,8 +15,8 @@ interface ItemProps {
   isActive?: boolean;
   isSearch?: boolean;
   level?: number;
-  onExpend?: () => void;
-  expended?: boolean;
+  onExpand?: () => void;
+  expanded?: boolean;
   label: string;
   onClick: () => void;
   icon: LucideIcon;
@@ -26,16 +31,46 @@ const Item = ({
   isActive,
   isSearch,
   level = 0,
-  onExpend,
-  expended,
+  onExpand,
+  expanded,
 }: ItemProps) => {
-  const ChevronIcon = expended ? ChevronDown : ChevronRight;
+  console.log(label, level);
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+
+  const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    onExpand?.();
+  };
+
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
+  const onCreate = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+
+    if (!id) return;
+
+    const promise = create({ title: "Untitled", parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+        // router.push(`/document/${documentId}`);
+
+        toast.promise(promise, {
+          loading: "Creating a new note..",
+          success: "Note created!",
+          error: "Failed to create a new note",
+        });
+      }
+    );
+  };
 
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
+      style={{ paddingLeft: level > 0 ? `${level * 12 + 12}px` : "12px" }}
       className={cn(
         "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
         isActive && "bg-primary/5 text-primary"
@@ -45,7 +80,7 @@ const Item = ({
         <div
           role="button"
           className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-          onClick={() => {}}
+          onClick={handleExpand}
         >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
@@ -61,8 +96,30 @@ const Item = ({
           <span className="text-xs">⌘</span>K
         </kbd>
       )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={onCreate}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{ paddingLeft: level ? `${level * 12 + 25}px` : "12px" }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="h-4 w-4"></Skeleton>
+      <Skeleton className="h-4 w-[30%]"></Skeleton>
+    </div>
+  );
+};
 export default Item;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -12,15 +12,15 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import Item from "./item";
 import { toast } from "sonner";
+import DocumentList from "./document-list";
 
 const Navigation = () => {
   const pathName = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
-  const documents = useQuery(api.documents.get);
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null);
@@ -140,11 +140,7 @@ const Navigation = () => {
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          <p>
-            {documents?.map((document) => (
-              <p key={document._id}>{document.title}</p>
-            ))}
-          </p>
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
