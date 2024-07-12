bc8db87078e98e9432bd4f721b0ffb7f1e45ff84
seomi <seomi.lee@gmail.com>
2024-07-12 20:41:53 +0900

    :hammer: icon picker 추가

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,35 @@
 "use client";
 
-const DocuementIdPage = () => {
-  return <div>Document Id</div>;
+import Toolbar from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentIdPageProps {
+  params: { documentId: Id<"documents"> };
+}
+
+const DocuementIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === null) {
+    return <div>Not found.</div>;
+  }
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:md-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocuementIdPage;
